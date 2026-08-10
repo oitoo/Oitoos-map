f37a954302e85feadc77e860630029251d90168d
oitoo <Joel.guitart@gmail.com>
2026-08-10 12:22:36 +0200

    Actualització de rutes públiques des de GeoRoute Studio

--- static/js/viewer.js
+++ static/js/viewer.js
@@ -9,7 +9,8 @@ const map = L.map("map", {
     zoomControl: true
 });
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+// Capa base clara / CartoDB Positron
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
 }).addTo(map);
